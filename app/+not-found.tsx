6196eda229a72cd00c589ee4463d00dc961c5df4
Mayra Moyano <mayramoyano2511@gmail.com>
2025-04-03 19:21:20 -0300

    tanda1

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,26 @@
+import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
-  return (
+    return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+    <View style={styles.container}>
+        <Link href="/" style={styles.button}>Go back to Home screen!</Link>
+    </View>
     </>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#25292e',
     justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+    alignItems: 'center',
+},
+button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+},
 });
